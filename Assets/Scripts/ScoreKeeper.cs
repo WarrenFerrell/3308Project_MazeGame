4c9df4b442b7c95101688f486e698b656d04824f
NathanMcKenna <namc8997@colorado.edu>
2015-06-16 12:47:39 -0600

    Added Scripts, Pause Menu
    added scripts for pause menu and score keeper

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public static int Score;
+	
+	void Awake()
+	{
+		Score = 0;
+	}
+	
+	void OnGUI()
+	{
+		GUI.Label(new Rect(0, 0, 100, 25), "Score: " + Score);
+	}
+}
