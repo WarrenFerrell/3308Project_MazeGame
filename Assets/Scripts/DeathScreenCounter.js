0fc3ce2012fb85bcb510c839ebb1ecd35f66ce68
NathanMcKenna <namc8997@colorado.edu>
2015-06-17 20:10:47 -0600

    Made additions to death screen
    added timer

---
+++ Assets/Scripts/DeathScreenCounter.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+ 
+ var theTimer : float = 0.0;
+ var theStartTime : float = 120.0;
+ var showRemaining : boolean = false;
+ var MyFont : Font;
+ 
+ function Start() 
+ {
+     theTimer = theStartTime;
+ }
+ 
+ function Update() 
+ {
+     theTimer -= Time.deltaTime;
+ 
+     if (theTimer < 10) 
+     {
+         Debug.Log("TEN SECONDS LEFT !");
+         showRemaining = true;
+     }
+ 
+     if (theTimer <= 0) 
+     {
+         Debug.Log("OUT OF TIME");
+         theTimer = 0;
+     }
+ 
+     if ( Input.GetKeyUp(KeyCode.G) )
+     {
+         Debug.Log("Resetting");
+         theTimer = theStartTime;
+         showRemaining = false;
+     }
+ }
+ 
+ function OnGUI() 
+ {
+     var text : String = String.Format( "{0:00}:{1:00}", parseInt( theTimer / 60.0 ), parseInt( theTimer % 60.0 ) ); 
+ 
+     if (showRemaining)
+     {
+     	GUI.skin.font = MyFont;
+         GUI.Label( Rect( 850, 500, Screen.width - 40, 60), text );
+     }
+ }
+ 
+ 
