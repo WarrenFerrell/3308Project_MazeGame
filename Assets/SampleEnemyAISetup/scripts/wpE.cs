1715af9b05e00ae5b96464c705b389fabc88b22c
Donovan.Guelde <dogu7023@colorado.edu>
2015-06-19 15:44:53 -0600

    modified 'sample enemy ai' waypoint E script
    continued contact with a waypoint results in continued change of enemy
    AI destination (dog can run around in a circle).  Implemented a check
    that will only generate a new destination if AI has reached its
    destination. This ensures AI can leave the current waypoint without
    receiving multiple destinations and ‘getting confused’

--- Assets/SampleEnemyAISetup/scripts/wpE.cs
+++ Assets/SampleEnemyAISetup/scripts/wpE.cs
@@ -2,19 +2,37 @@
 using System.Collections;
 
 public class wpE : MonoBehaviour {
-	
+
+	string collidingWithName;
+	string collidedWithLastTime;
 	string[] possibleDestinations = {"wpB","wpC","wpD","wpA"};
 	GameObject collidingWith;
-	
 	public string destination;
+	GameObject prevDestination;
+
+
+
+	void previousDestination(GameObject previousDestination)
+	{
+		prevDestination = previousDestination;
+	}
 	
 	void OnTriggerEnter(Collider collision)
 	{
+		if (gameObject == prevDestination)
+		{
 		collidingWith = collision.gameObject;
+		collidingWithName = collidingWith.name;
+
 		do
 		{int index = Random.Range (0,4);
 			destination = possibleDestinations[index];}
 		while (destination=="NULL");
 		collidingWith.SendMessage("nextDestination",GameObject.Find (destination));
+		}
 	}
+
+
+
+
 }
