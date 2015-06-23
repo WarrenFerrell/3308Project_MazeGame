5df9f00ee0926512fb171a29127216391550b62c
NathanMcKenna <namc8997@colorado.edu>
2015-06-22 21:08:52 -0600

    Added footstep audio
    Added footstep sound file and edited footstep script

--- Assets/Scripts/Footsteps.js
+++ Assets/Scripts/Footsteps.js
@@ -1,9 +1,22 @@
 ﻿#pragma strict
 
+var AudioFile:AudioClip;
+
 function Start () {
 
 }
 
 function Update () {
-
+	if (Input.GetKeyDown (KeyCode.W))
+ 	{
+     	GetComponent.<AudioSource>().clip = AudioFile;
+     	GetComponent.<AudioSource>().Play();
+ 
+ 	}
+ 	if (Input.GetKeyUp (KeyCode.W))
+ 	{
+     	GetComponent.<AudioSource>().clip = AudioFile;
+     	GetComponent.<AudioSource>().Stop();
+ 
+ 	}
 }
