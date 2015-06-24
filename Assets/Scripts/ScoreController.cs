acab7965d68597671f114babaa20907a4d9116e1
Warren Ferrell <warren.ferrell@colorado.edu>
2015-06-24 11:00:47 -0600

    Revert "Added CollectibleObject, ScoreController, and ScoreKeeper scripts."
    This reverts commit e87ce5e8317ad55ddc1ec184ad7cb419bd1dc1ea.

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -3,40 +3,21 @@ using System.Collections;
 
 public class ScoreController : MonoBehaviour
 {
-	private string secretKey = "csci3308Proj"; // Value should be same as one on server
-	public string addScoreURL = "~mitchellblock/mazeHighScore.html"; //be sure to add a ? to your url
-	public string highscoreURL = "~mitchellblock/mazeHighScore.html";
+	/*private string secretKey = "mySecretKey"; // Edit this value and make sure it's the same as the one stored on the server
+	public string addScoreURL = ; //be sure to add a ? to your url
+	public string highscoreURL = ;
 	
 	void Start()
 	{
 		StartCoroutine(GetScores());
 	}
-
-	public string Md5Sum(string strToEncrypt)
-	{
-		System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
-		byte[] bytes = ue.GetBytes(strToEncrypt);
-		
-		// encrypt bytes
-		System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
-		byte[] hashBytes = md5.ComputeHash(bytes);
-		
-		// Convert the encrypted bytes back to a string (base 16)
-		string hashString = "";
-		
-		for (int i = 0; i < hashBytes.Length; i++)
-		{
-			hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
-		}
-		
-		return hashString.PadLeft(32, '0');
-	}
+	
 	// remember to use StartCoroutine when calling this function!
 	IEnumerator PostScores(string name, int score)
 	{
 		//This connects to a server side php script that will add the name and score to a MySQL DB.
 		// Supply it with a string representing the players name and the players score.
-		string hash = Md5Sum(name + score + secretKey);
+		string hash = MD5Test.Md5Sum(name + score + secretKey);
 		
 		string post_url = addScoreURL + "name=" + WWW.EscapeURL(name) + "&score=" + score + "&hash=" + hash;
 		
@@ -54,8 +35,7 @@ public class ScoreController : MonoBehaviour
 	// remember to use StartCoroutine when calling this function!
 	IEnumerator GetScores()
 	{
-		GUIText scoreLoad = gameObject.GetComponent<GUIText>();
-		scoreLoad.text = "Loading Scores";
+		gameObject.guiText.text = "Loading Scores";
 		WWW hs_get = new WWW(highscoreURL);
 		yield return hs_get;
 		
@@ -65,8 +45,8 @@ public class ScoreController : MonoBehaviour
 		}
 		else
 		{
-			scoreLoad.text = hs_get.text; // this is a GUIText that will display the scores in game.
+			gameObject.guiText.text = hs_get.text; // this is a GUIText that will display the scores in game.
 		}
-	}
+	}*/
 	
 }
--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -3,66 +3,15 @@ using System.Collections;
 
 public class ScoreKeeper : MonoBehaviour {
 
-	public int Score;
+	public static int Score;
 	
 	void Awake()
 	{
 		Score = 0;
 	}
-
-	void OnGUI() {
-
-	}
-
-}
-
-/*using UnityEngine;
-using System.Collections;
-	public void AddScore (int newScoreValue)
-	{
-		Score += newScoreValue;
-		UpdateScore ();
-	}
 	
-	void UpdateScore ()
+	void OnGUI()
 	{
-		scoreText.text = "Score: " + Score;
+		GUI.Label(new Rect(0, 0, 100, 25), "Score: " + Score);
 	}
 }
-
-public class DestroyByContact : MonoBehaviour
-{
-    public GameObject explosion;
-    public GameObject playerExplosion;
-    public int scoreValue;
-    private GameController gameController;
-
-    void Start ()
-    {
-        GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
-        if (gameControllerObject != null)
-        {
-            gameController = gameControllerObject.GetComponent <GameController>();
-        }
-        if (gameController == null)
-        {
-            Debug.Log ("Cannot find 'GameController' script");
-        }
-    }
-
-    void OnTriggerEnter(Collider other) 
-    {
-        if (other.tag == "Boundary")
-        {
-            return;
-        }
-        Instantiate(explosion, transform.position, transform.rotation);
-        if (other.tag == "Player")
-        {
-            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-        }
-        gameController.AddScore (scoreValue);
-        Destroy(other.gameObject);
-        Destroy(gameObject);
-    }
-}*/
