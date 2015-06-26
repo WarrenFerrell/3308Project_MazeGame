f80fb239be4f37292d993fbe32d4c36f54890292
NathanMcKenna <namc8997@colorado.edu>
2015-06-26 11:08:12 -0600

    Started javascript documentation

--- Assets/Scripts/GameMenu.js
+++ Assets/Scripts/GameMenu.js
@@ -1,33 +1,38 @@
 ﻿var isQuit=false;
 var AudioFile:AudioClip;
 
+/*
+Function: OnMouseEnter
+	Plays audio file and changes text color if the user's mouse contacts a button
+*/
 function OnMouseEnter(){
-//Play Music
-GetComponent.<AudioSource>().clip = AudioFile;
-GetComponent.<AudioSource>().Play();
-//change text color
-GetComponent.<Renderer>().material.color=Color.red;
+	//Play Music
+	GetComponent.<AudioSource>().clip = AudioFile;
+	GetComponent.<AudioSource>().Play();
+	//change text color
+	GetComponent.<Renderer>().material.color=Color.red;
 }
 
 function OnMouseExit(){
-//change text color
-GetComponent.<Renderer>().material.color=Color.blue;
+	//change text color
+	GetComponent.<Renderer>().material.color=Color.blue;
 }
 
 function OnMouseUp(){
-//is this quit
-if (isQuit==true) {
-//quit the game
-Application.Quit();
-}
-else {
-//load level
-Application.LoadLevel("StaticMaze");
-}
+	//is this quit
+	if (isQuit==true) {
+	//quit the game
+		Application.Quit();
+	}
+	else {
+	//load level
+		Application.LoadLevel("StaticMaze");
+	}
 }
 
 function Update(){
 //quit game if escape key is pressed
-if (Input.GetKey(KeyCode.Escape)) { Application.Quit();
-}
+	if (Input.GetKey(KeyCode.Escape))
+		{ Application.Quit();
+	}	
 }
