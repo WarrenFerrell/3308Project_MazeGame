b0aae78f944794e57232603272df9483a893089f
NathanMcKenna <namc8997@colorado.edu>
2015-06-26 10:30:35 -0600

    Added print score script to death screen

---
+++ Assets/Scripts/PrintScore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintScore : MonoBehaviour {
+
+	public GUIText scoreText;
+
+	// Use this for initialization
+	void Start () {
+		scoreText.text = "" + ScoreKeeper.Score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
