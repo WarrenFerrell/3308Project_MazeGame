18a4c8020ed33460d252f2f7ea3393c6a0b6362e
Donovan.Guelde <dogu7023@colorado.edu>
2015-06-26 12:33:47 -0600

    deleted unused c# script, commented enemyAI script

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -1,30 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*! Script name: enemyAI.cs
+ * Description: enemyAI script is attached to the enemy game object 'labrador'
+ * this script receives information from maze pieces regarding possible destinations
+ * from that location.  EnemyAI script chooses random destination from possible destinations
+ * */
+
 public class enemyAI : MonoBehaviour {
 	
-	Transform destinationTransform;
+	Transform destinationTransform;			
 	NavMeshAgent nav;
 	public GameObject collidingWith;
 	public string destination;
 	public string previousDestination;
 	public string[] destinationArray;
 	bool arrayFilled;
-
 	GameObject destinationObject;
 	string temp;
 	
-	void Start ()
-	{
-		destinationArray = new string[4];
-		destination = "startTile";
+	void Start ()  //! Start function excuted one time, upon enemy object creation, initializes destination array
+	{   destinationArray = new string[4]; 
+		//destination = "startTile";
 		previousDestination = "NULL";
 		nav = GetComponent <NavMeshAgent> ();
-		arrayFilled=false;
-		
-	}
+		arrayFilled=false;}
 
-
+	/*!fillArray functions receive information via Unity-specific 'SendMessage' command,
+	 * and fill array with possible destinations from current location, 
+	 and sets boolean arrayFilled to true*/
 	void fillArray0(string fromMazeScript)
 	{destinationArray[0] = fromMazeScript;}
 
@@ -38,33 +42,38 @@ public class enemyAI : MonoBehaviour {
 	{destinationArray[3] = fromMazeScript;
 		arrayFilled=true;}
 
-
-	void OnCollisionEnter(Collision collision) //check if collision is with player
+	/*! OnCollisionEnter() checks if the enemy collides with the player, 
+	 * and if so, load the 'deathScreen' scene*/
+	void OnCollisionEnter(Collision collision) 
 	{
 		if (collision.gameObject.name == "Dark_Cat_Prefab")
 		{Application.LoadLevel("DeathScreen");} //redirect to death screen
 	}
 
-	//enter this function upon 'trigger' by another game object
+	/*!OnTriggerEnter function is called when the enemy collides with another object.  Performs a check
+	 * to see if array of destinations has been filled, and if so, choose a destination, and store previous 
+	 * destination*/
 	void OnTriggerEnter(Collider collision)
 	{
 		collidingWith = collision.gameObject;
 		if (arrayFilled==true)
 		{
-		do 
+			do 
 			{temp = destinationArray[Random.Range (0,4)];}
-		while (temp=="NULL" || temp == previousDestination);
-
+			while (temp=="NULL" || temp == previousDestination);
 		previousDestination = destination;
 		destination = temp;
-			}
+		}
 	}
 
+
+	/*!OnCollisionExit called when enemy object separated from the objet it collided with, initiating
+	 * OnTriggerEnter().  Resets boolean variable arrayFilled to false*/
 	void OnCollisionExit(Collision collision)
 	{
 		arrayFilled=false;
 	}
 	
-	// Update is called once per frame
+	//! Update() sets the destination to that chosen in OnTriggerEnter()
 	void Update () {nav.SetDestination(GameObject.Find (destination).transform.position);}
 }
